f7d7cec900085ca209ff8155a45b484d8ad84d6e
will chandler <jaskamiin@gmail.com>
2025-05-13 01:21:38 -0400

    debug: pathing still not working

--- cli/setup.py
+++ cli/setup.py
@@ -25,7 +25,7 @@ setup(
     python_requires=">=3.10",
     entry_points={
         'console_scripts': [
-            'atra=__main__:main',
+            'atra=cli.__main__:main',
         ],
     },
     classifiers=[
