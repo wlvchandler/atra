1aa9489332904794477dc6a3fe39cb39789652d8
will chandler <jaskamiin@gmail.com>
2025-05-14 05:20:51 -0400

    feat: dockerize atra-gateway

--- atra-gateway/lib/atra_gateway/grpc_connection.ex
+++ atra-gateway/lib/atra_gateway/grpc_connection.ex
@@ -7,7 +7,13 @@ defmodule AtraGateway.GrpcConnection do
   end
 
   def init(_) do
-    case GRPC.Stub.connect("localhost:50051", pool_size: 1) do
+    matcher_host = System.get_env("MATCHER_HOST", "localhost")
+    matcher_port = System.get_env("MATCHER_PORT", "50051")
+
+    connection_string = "#{matcher_host}:#{matcher_port}"
+    Logger.info("Connecting to matcher at #{connection_string}")
+    
+    case GRPC.Stub.connect(connection_string, pool_size: 1) do
       {:ok, channel} ->
         Logger.info("Connected to matching engine")
         {:ok, %{channel: channel}}
